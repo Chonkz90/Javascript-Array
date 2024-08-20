d3b5b7fc9868e2e7b60b8ddfb61b43f96585128d
Chonkz90 <zahlerdal@yahoo.com>
2024-08-20 11:16:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Javascript-Array
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Chonkz90/Javascript-Array)
---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript Array
+
+const userName = "Tomas";
+
+const userArray = ["Tomas", "Erdal", 34, true];
+console.log(userArray);
+console.log(userArray[2]);
